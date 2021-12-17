3d7cc3958b6355b07015bd960a17a7636b821880
codinghubgit <codinghubgit2021@gmail.com>
2021-12-16 18:42:10 -0800

    fourth commit

--- script.py
+++ script.py
@@ -3,11 +3,13 @@ import sys
 print(sys.version)
 print(sys.executable)
 
+
 def greet(who_to_greet):
-    greeting='Hello, {}'.format(who_to_greet)
+    greeting = "Hello, {}".format(who_to_greet)
     return greeting
 
 
-print(greet('python_project'))
-print(greet('python_project'))
-print(greet('python_project'))
+print(greet("python_project"))
+print(greet("python_project"))
+print(greet("python_project"))
+print(sys.executable)
